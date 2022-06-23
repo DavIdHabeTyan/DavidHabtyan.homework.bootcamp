c7a00b2485bb034986c48ad7ae20587bfe2ddf88
David Habetyan <davidhabetyanm@gmail.com>
2022-06-23 21:22:23 +0400

    added test for homework

--- Homework1/main.test.js
+++ Homework1/main.test.js
@@ -2,18 +2,27 @@
 
 describe('1. Write a function that checks if the number is palindrome(121, 23432), dont use strings, and work with numbers.',  () => {
    function palindrome(numb){
-      let result = ""
-      while(0 < numb){
-         let lastNumb = numb%10;
-         result+=lastNumb;
-         lastNumb = (numb % 10) /10;
+      let number = numb;
+      let lastNumber;
+      let result = '';
+      while(number > 0) {
+         lastNumber = number % 10;
+         result += lastNumber;
+         number = (number - lastNumber) /10
       }
-      return result
+      return numb === +result
    }
 
-   test('test 1.', () => {
+   test('Test 1. check number 121', () => {
       expect(palindrome(121)).toEqual(true);
    })
    
-   
+   test('Test 2. check number 23432', () => {
+      expect(palindrome(23432)).toEqual(true);
+   })
+
+   test('Test 3. check number 122', () => {
+      expect(palindrome(122)).toEqual(false);
+   })
+
  });
