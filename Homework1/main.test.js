b0c426ba4b0d52f2a851a0d283858e7045626c45
David Habetyan <davidhabetyanm@gmail.com>
2022-06-24 18:02:36 +0400

    added exercises

--- Homework1/main.test.js
+++ Homework1/main.test.js
@@ -47,7 +47,6 @@ describe('1. Write a function that checks if the number is palindrome(121, 23432
       expect(checkNumberIsPalindrome(23432)).toEqual(true)
    })
 
-
 });
 
 describe('2. Write a function that finds the first 3 "Happy" numbers (p.s  number is happy if the sum of the dividers(not including the number itself) is equal to the number itself i.e 6 = 1 + 2 + 3,  28 = 1 + 2 + 4 + 7 + 14).', () => {
@@ -65,7 +64,7 @@ describe('2. Write a function that finds the first 3 "Happy" numbers (p.s  numbe
       expect(findNumberIsHappy(6)).toEqual(true);
    });
 
-   test( 'Test 2. Happy Number', () => {
+   test('Test 2. Happy Number', () => {
       expect(findNumberIsHappy(28)).toEqual(true);
    })
 
@@ -74,3 +73,61 @@ describe('2. Write a function that finds the first 3 "Happy" numbers (p.s  numbe
    })
 
 });
+
+describe(' Write a method that creates a new array with given values ', () => {
+   function creatNewArr(numb, leter) {
+      let result = [];
+      for (let i = 0; i < numb; i++) {
+         result.push(leter)
+      }
+      return result
+   }
+
+   test('Test 1.', () => {
+      expect(creatNewArr(3, 'a')).toEqual(['a', 'a', 'a'])
+   })
+
+   test('Test 2', () => {
+      expect(creatNewArr(4, 'b')).toEqual(['b', 'b', 'b', 'b'])
+   })
+
+});
+
+
+describe('Reverse. Write a function that reverts the input array.', () => {
+   function revers(arr) {
+      let result = [];
+      for (let i = 0; i < arr.length; i++) {
+         result.unshift(arr[i]);
+      }
+      return result
+   }
+
+   test('Test 1', () => {
+      expect(revers([1, 2, 3])).toEqual([3, 2, 1]);
+   })
+
+   test('Test 2', () => {
+      expect(revers([4, 5, 6])).toEqual([6, 5, 4])
+   })
+
+});
+
+describe('Write a method that clears array from all unnecessary elements, like false, undefined, empty strings, zero, null', () => {
+   function clearArray(arr) {
+      let result = [];
+      for (let i = 0; i < arr.length; i++) {
+         if(arr[i] === 0) continue;
+         if(typeof arr[i] === 'number'){
+            result.push(arr[i])
+         }
+      }
+      return result
+
+   }
+
+   test('Test 1', () => {
+      expect(clearArray([0, 1, false, 2, undefined, '', 3, null])).toEqual([1, 2, 3]);
+   })
+
+})
