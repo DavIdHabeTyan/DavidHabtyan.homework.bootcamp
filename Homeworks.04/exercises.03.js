ce28ec7e8b594956bfaabef7232339b610de67ca
David Habetyan <davidhabetyanm@gmail.com>
2022-08-10 13:32:39 +0400

    added exercises 03, 05

---
+++ Homeworks.04/exercises.03.js
@@ -0,0 +1,20 @@
+// Exercise 3 Abstract class
+
+class Abstract{
+   constructor() {
+      if(this.constructor.name === "Abstract"){
+         throw new Error("Abstract class cannot be instanced");
+      }
+   }
+}
+
+class Substract extends Abstract{
+   constructor() {
+      super();
+      this.type = "substract"
+   }
+}
+
+let sub = new Substract()
+console.log(sub);
+console.log(sub.constructor.name)
---
+++ Homeworks.04/exercises.06.js
@@ -0,0 +1,48 @@
+// 06. Create a TV class with properties like brand, channel and volume.
+
+class TV {
+   constructor(brand, channel = 1, volume = 50) {
+      this.brand = brand;
+      this.channel = channel;
+      this.volume = volume;
+
+   }
+
+   increaseVolume = function () {
+      if (this.volume > 0 && this.volume < 100) {
+         return this.volume++
+      } else {
+         return "volume or minimum 0 or maximum 100"
+      }
+
+   }
+   decreaseVolume = () => {
+      if (this.volume >= 0 && this.volume <= 100) {
+         return this.volume--
+      } else {
+         return "volume or minimum 0 or maximum 100"
+      }
+   }
+
+   setChannel = (set) => {
+      return this.channel = set
+   }
+
+   ////// ??????????
+   reset = () => {
+
+   }
+
+   status = () => {
+      return `${this.brand} at channel ${this.channel}, volume ${this.volume}`
+   }
+}
+
+let tv = new TV("Samsung", 10, 5);
+console.log(tv.setChannel(60))
+
+console.log(tv.decreaseVolume())
+console.log(tv.decreaseVolume())
+console.log(tv.decreaseVolume())
+
+console.log(tv.status())
